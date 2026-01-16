03ff9f04141df951aae7b0f8cc81149d05a47579
Randa-hb10 <rm-400000@hotmail.com>
2026-01-16 20:18:36 +0300

    function that checks for lowercase character fixed

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,24 +1,4 @@
 #!/usr/bin/python3
-
 def islower(c):
-    """Check if a character is lowercase."""
-    if len(c) == 0:
-        return False
-    
-    ascii_val = ord(c[0])
-    
-    # Check if ASCII value is between 'a' (97) and 'z' (122)
-    if 97 <= ascii_val <= 122:
-        return True
-    else:
-        return False
-
-
-# Code to allow direct execution
-if __name__ == "__main__":
-    # Test cases from the example
-    print("a is {}".format("lower" if islower("a") else "upper"))
-    print("H is {}".format("lower" if islower("H") else "upper"))
-    print("A is {}".format("lower" if islower("A") else "upper"))
-    print("3 is {}".format("lower" if islower("3") else "upper"))
-    print("g is {}".format("lower" if islower("g") else "upper"))
+    """Check for lowercase character."""
+    return ord('a') <= ord(c) <= ord('z')
