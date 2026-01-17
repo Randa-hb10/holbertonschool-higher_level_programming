f0ff3bca414dd61cad0462473c996d5d25e63011
Randa-hb10 <rm-400000@hotmail.com>
2026-01-17 20:15:06 +0300

    Discover hidden names from compiled module

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    hidden = __import__("hidden_4")
 
-    for name in dir(hidden):
+if __name__ == "__main__":
+    hidden_4 = __import__("hidden_4")
+
+    for name in sorted(dir(hidden_4)):
         if not name.startswith("__"):
             print(name)
