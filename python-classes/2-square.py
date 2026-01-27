f9a4d344c854af16f223e3337f9d7520dd6d3fca
Randa-hb10 <rm-400000@hotmail.com>
2026-01-27 11:28:12 +0300

    add new line and new commit

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""Defines a Square class with a private size attribute."""
+"""Defines a Square class with size validation."""
 
 
 class Square:
-    """Represents a square with a private size attribute
+    """Represents a square with a private size attribute."""
+
+    def __init__(self, size=0):
+        """Initializes a Square instance.
 
         Args:
             size (int): Size of the square.
@@ -11,12 +14,10 @@ class Square:
         Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
-    """
-
-    def __init__(self, size=0):
-        """Initialize the square with optional size, validating type and value."""
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
         self.__size = size
