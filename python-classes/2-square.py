5c7ffa867e8653678f6d252cf3fbce563f95e334
Randa-hb10 <rm-400000@hotmail.com>
2026-01-27 09:56:14 +0300

    Initialize the square with optional size, validating type and value

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a Square class with a private size attribute."""
+
+class Square:
+    """Represents a square with a private size attribute."""
+
+    def __init__(self, size=0):
+        """Initialize the square with optional size, validating type and value."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
