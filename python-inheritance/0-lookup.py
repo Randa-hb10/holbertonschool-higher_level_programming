9bfe361da55b0466f37da1ee10e03585844f5bd5
Randa-hb10 <rm-400000@hotmail.com>
2026-01-28 23:17:32 +0300

    lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+0-lookup.py
+
+This module contains a function `lookup` that returns the list of
+available attributes and methods of any Python object.
+"""
 def lookup(obj):
     """
     Return the list of available attributes and methods of an object.
